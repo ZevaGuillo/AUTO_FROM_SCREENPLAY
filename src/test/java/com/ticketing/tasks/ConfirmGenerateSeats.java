03439629d0f7ae45261bd8dde058ce3b888e56e9
ZevaGuillo <guiller.zeva16@gmail.com>
2026-03-20 12:57:52 -0500

    feat: Add wait condition for seat generation button visibility

--- src/test/java/com/ticketing/tasks/ConfirmGenerateSeats.java
+++ src/test/java/com/ticketing/tasks/ConfirmGenerateSeats.java
@@ -3,6 +3,8 @@ package com.ticketing.tasks;
 import com.ticketing.ui.SeatManagementPage;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.JavaScriptClick;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class ConfirmGenerateSeats implements Task {
 
@@ -13,6 +15,7 @@ public class ConfirmGenerateSeats implements Task {
     @Override
     public <T extends net.serenitybdd.screenplay.Actor> void performAs(T actor) {
         actor.attemptsTo(
+                WaitUntil.the(SeatManagementPage.GENERATE_BUTTON, isVisible()).forNoMoreThan(10).seconds(),
                 JavaScriptClick.on(SeatManagementPage.GENERATE_BUTTON)
         );
     }
