ebfe92ce677085bb6a953b83d2d058128ff3a19a
ZevaGuillo <guiller.zeva16@gmail.com>
2026-03-20 14:02:27 -0500

    feat: Add wait condition for events table visibility in NavigateToEvents task

--- src/test/java/com/ticketing/tasks/NavigateToEvents.java
+++ src/test/java/com/ticketing/tasks/NavigateToEvents.java
@@ -3,6 +3,9 @@ package com.ticketing.tasks;
 import com.ticketing.ui.AdminEventsPage;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class NavigateToEvents implements Task {
 
@@ -13,7 +16,8 @@ public class NavigateToEvents implements Task {
     @Override
     public <T extends net.serenitybdd.screenplay.Actor> void performAs(T actor) {
         actor.attemptsTo(
-                Open.url("http://localhost:3000/admin/events")
+                Open.url("http://localhost:3000/admin/events"),
+                WaitUntil.the(AdminEventsPage.EVENTS_TABLE, isVisible()).forNoMoreThan(5).seconds()
         );
     }
 }
